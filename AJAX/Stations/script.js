c7a0e9f522e782ea4e3c1710c38454bf3825dde4
Ofir Patish <ofirpatishop@gmail.com>
2024-02-06 20:26:14 +0200

    Add Leaflet map and update search functionality

--- AJAX/Stations/script.js
+++ AJAX/Stations/script.js
@@ -1,27 +1,31 @@
 $(document).ready(function () {
+  var map = L.map("map").setView([32.8417945, 35.0819335], 14);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  var markers = L.layerGroup().addTo(map);
+
   $("#searchButton").click(function () {
-    var neighborhood = $("#citySearch").val();
+    var searchedCity = $("#citySearch").val();
     var apiUrl =
       "https://data.gov.il/api/3/action/datastore_search?resource_id=e873e6a2-66c1-494f-a677-f5e77348edb0&q=" +
-      neighborhood;
+      searchedCity;
 
     $.ajax({
       url: apiUrl,
       type: "GET",
       success: function (response) {
-        var records = response.result.records;
-        $("#tableBody").empty();
+        markers.clearLayers();
+        var cityDataRecords = response.result.records;
 
-        for (var index = 0; index < records.length; index++) {
-          var row =
-            "<tr><td>" +
-            records[index].Neighborhood +
-            "</td><td>" +
-            records[index].Lat +
-            "</td><td>" +
-            records[index].Long +
-            "</td></tr>";
-          $("#tableBody").append(row);
+        for (var index = 0; index < cityDataRecords.length; index++) {
+          var singleCityData = cityDataRecords[index];
+          if (singleCityData.Lat && singleCityData.Long && singleCityData.CityName === searchedCity) {
+            var marker = L.marker([singleCityData.Lat, singleCityData.Long]).addTo(markers);
+            marker.bindPopup("<b>City:</b> " + singleCityData.StationTypeName);
+          }
         }
       },
       error: function (error) {
