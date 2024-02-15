cdf0be2191ae391114c827a0f6629da9e50576fc
Ofir <ofirpatishop@gmail.com>
2024-02-15 23:55:49 +0200

    Refactor country search form and style

--- Assignment/Task (B)/js/script.js
+++ Assignment/Task (B)/js/script.js
@@ -1,22 +1,18 @@
-// Wait for the document to be ready
 $(document).ready(function () {
-  // Event handler for the "all-button" click event
   $("#all-button").click(function () {
-    // AJAX GET request to the REST Countries API
     $.ajax({
       url: "https://restcountries.com/v3.1/all",
       type: "GET",
       success: function (data) {
-        // Calculate total population of all countries
         let totalPopulation = 0;
         for (let index = 0; index < data.length; index++) {
           totalPopulation += data[index].population;
         }
 
-        // Calculate average population of all countries
-        let averagePopulation = totalPopulation / data.length;
+        // Calculate average population
+        let averagePopulation = Math.round(totalPopulation / data.length);
 
-        // Calculate number of countries per region
+        // Count countries per region
         let regionCounts = {};
         data.forEach(function (country) {
           if (country.region in regionCounts) {
@@ -26,105 +22,95 @@ $(document).ready(function () {
           }
         });
 
-        // Prepare HTML to display the data
-        let html = "<p>Total countries: " + data.length + "</p>";
-        html += "<p>Total population: " + totalPopulation + "</p>";
-        html += "<p>Average population: " + averagePopulation + "</p>";
+        // Construct HTML for data display
+        let html = "<p class='data-display'>Total countries: " + data.length + "</p>";
+        html += "<p class='data-display'>Total population: " + totalPopulation + "</p>";
+        html += "<p class='data-display'>Average population: " + averagePopulation + "</p>";
 
-        // Prepare HTML for the table of countries
-        html += "<table><tr><th>Country Name</th><th>Number of Citizens</th></tr>";
+        // Construct HTML for countries table
+        html += "<table class='table'><thead><tr><th>Country Name</th><th>Number of Citizens</th></tr></thead><tbody>";
         data.forEach(function (country) {
           html += "<tr><td>" + country.name.common + "</td><td>" + country.population + "</td></tr>";
         });
-        html += "</table>";
+        html += "</tbody></table>";
 
-        // Prepare HTML for the table of regions
-        html += "<table><tr><th>Region</th><th>Number of Countries</th></tr>";
+        // Construct HTML for regions table
+        html += "<table class='table'><thead><tr><th>Region</th><th>Number of Countries</th></tr></thead><tbody>";
         for (let region in regionCounts) {
           html += "<tr><td>" + region + "</td><td>" + regionCounts[region] + "</td></tr>";
         }
-        html += "</table>";
+        html += "</tbody></table>";
 
-        // Display the prepared HTML
-        $("#country-data").html(html).show();
+        // Display data
+        $("#country-data").html(html);
       },
       error: function (error) {
-        // Log any errors
         console.log("Error:", error);
       },
     });
   });
 
-  // Event handler for the "search-button" click event
-  $("#search-button").click(function (clickEvent) {
-    // Prevent the default form submission
-    clickEvent.preventDefault();
+  $("#country-search-form").submit(function (submitEvent) {
+    submitEvent.preventDefault();
+
     let countryName = $("#country-name").val();
-    if (countryName) {
-      // AJAX GET request to the REST Countries API for a specific country
-      $.ajax({
-        url: "https://restcountries.com/v3.1/name/" + countryName,
-        type: "GET",
-        success: function (data) {
-          if (data.length > 0) {
-            // Calculate total population of the searched countries
-            let totalPopulation = 0;
-            for (let index = 0; index < data.length; index++) {
-              totalPopulation += data[index].population;
-            }
+    // Clear the input field
+    $("#country-name").val("");
 
-            // Calculate average population of the searched countries
-            let averagePopulation = totalPopulation / data.length;
-
-            // Calculate number of searched countries per region
-            let regionCounts = {};
-            data.forEach(function (country) {
-              if (country.region in regionCounts) {
-                regionCounts[country.region]++;
-              } else {
-                regionCounts[country.region] = 1;
-              }
-            });
-
-            // Prepare HTML to display the data
-            let html = "<p>Total countries: " + data.length + "</p>";
-            html += "<p>Total population: " + totalPopulation + "</p>";
-            html += "<p>Average population: " + averagePopulation + "</p>";
-
-            // Prepare HTML for the table of countries
-            html += "<table><tr><th>Country Name</th><th>Number of Citizens</th></tr>";
-            data.forEach(function (country) {
-              html += "<tr><td>" + country.name.common + "</td><td>" + country.population + "</td></tr>";
-            });
-            html += "</table>";
-
-            // Prepare HTML for the table of regions
-            html += "<table><tr><th>Region</th><th>Number of Countries</th></tr>";
-            for (let region in regionCounts) {
-              html += "<tr><td>" + region + "</td><td>" + regionCounts[region] + "</td></tr>";
-            }
-            html += "</table>";
-
-            // Display the prepared HTML
-            $("#country-data").html(html).show();
-          } else {
-            // Hide the country data if no country was found
-            $("#country-data").hide();
-            console.log("Country not found");
+    $.ajax({
+      url: "https://restcountries.com/v3.1/name/" + countryName,
+      type: "GET",
+      success: function (data) {
+        if (data.length > 0) {
+          let totalPopulation = 0;
+          for (let index = 0; index < data.length; index++) {
+            totalPopulation += data[index].population;
           }
-        },
-        error: function (error) {
-          // Log any errors
-          console.log("Error:", error);
-        },
-      });
-    } else {
-      // Highlight the input field in red if no country name was entered
-      $("#country-name").css("border", "1px solid red");
-      // Remove the red border after X seconds
-      setTimeout(function () {
-        $("#country-name").css("border", "");
-      }, 3000);
-    }
+
+          // Calculate average population
+          let averagePopulation = Math.round(totalPopulation / data.length);
+
+          // Count countries per region
+          let regionCounts = {};
+          data.forEach(function (country) {
+            if (country.region in regionCounts) {
+              regionCounts[country.region]++;
+            } else {
+              regionCounts[country.region] = 1;
+            }
+          });
+
+          // Construct HTML for data display
+          let html = "<p class='data-display'>Total countries: " + data.length + "</p>";
+          html += "<p class='data-display'>Total population: " + totalPopulation + "</p>";
+          html += "<p class='data-display'>Average population: " + averagePopulation + "</p>";
+
+          // Construct HTML for countries table
+          html +=
+            "<table class='table'><thead><tr><th>Country Name</th><th>Number of Citizens</th></tr></thead><tbody>";
+          data.forEach(function (country) {
+            html += "<tr><td>" + country.name.common + "</td><td>" + country.population + "</td></tr>";
+          });
+          html += "</tbody></table>";
+
+          // Construct HTML for regions table
+          html += "<table class='table'><thead><tr><th>Region</th><th>Number of Countries</th></tr></thead><tbody>";
+          for (let region in regionCounts) {
+            html += "<tr><td>" + region + "</td><td>" + regionCounts[region] + "</td></tr>";
+          }
+          html += "</tbody></table>";
+
+          // Display data
+          $("#country-data").html(html);
+        } else {
+          console.log("Country not found");
+          $("#country-data").html("<p class='error-message'>Country not found</p>");
+        }
+      },
+      error: function (error) {
+        $("#country-data").html("<p class='error-message'>Country not found</p>");
+        console.log("Error:", error);
+      },
+    });
   });
 });
