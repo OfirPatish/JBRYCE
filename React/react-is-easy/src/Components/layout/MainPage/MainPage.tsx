fa7397e57715258afd6601e9f2188ddb3b025fba
Ofir Patish <ofirpatishop@gmail.com>
2024-03-01 12:57:17 +0200

    Remove Gang2 component and update MainPage.tsx

--- React/react-is-easy/src/Components/layout/MainPage/MainPage.tsx
+++ React/react-is-easy/src/Components/layout/MainPage/MainPage.tsx
@@ -2,7 +2,6 @@ import { spawn } from "child_process";
 import Ofir from "../../stam/Ofir/Ofir";
 import "./MainPage.css";
 import Gang1 from "../../Modules/Gang1/Gang1";
-import Gang2 from "../../Modules/Gang2/Gang2";
 
 function MainPage(): JSX.Element {
   //   let bestStudent: string = "Matti";
@@ -22,30 +21,26 @@ function MainPage(): JSX.Element {
   //     { id: 6, name: "Dima" },
   //   ];
   let myDevice = [
-    { id: 1, nodeID: 10, name: "entrance", location: "main-floor", epID: 100 },
-    { id: 2, nodeID: 12, name: "dinning", location: "main-floor", epID: 101 },
-    { id: 3, nodeID: 14, name: "kitchen", location: "main-floor", epID: 102 },
-    { id: 4, nodeID: 16, name: "living-room", location: "main-floor", epID: 103 },
-    { id: 5, nodeID: 18, name: "bathroom", location: "main-floor", epID: 104 },
+    { id: 1, nodeID: 10, name: "entrance", location: "main-floor", totalepID: 1 },
+    { id: 2, nodeID: 12, name: "dinning", location: "main-floor", totalepID: 1 },
+    { id: 3, nodeID: 14, name: "kitchen", location: "main-floor", totalepID: 1 },
+    { id: 4, nodeID: 16, name: "living-room", location: "main-floor", totalepID: 2 },
+    { id: 5, nodeID: 18, name: "bathroom", location: "main-floor", totalepID: 2 },
   ];
 
-  let myDeviceWithEpID = [
-    { id: 6, nodeID: 20, name: "bedroom", location: "second-floor", epID: 105 },
-    { id: 7, nodeID: 22, name: "bathroom", location: "second-floor", epID: 106 },
-    { id: 8, nodeID: 24, name: "office", location: "second-floor", epID: 107 },
-    { id: 9, nodeID: 26, name: "kids-room", location: "second-floor", epID: 108 },
-    { id: 10, nodeID: 28, name: "guest-room", location: "second-floor", epID: 109 },
-  ];
   return (
     <div className="MainPage">
       {/* <Gang1 nodeID={10} name={"entrance"} location={"main-floor"} />
       <Gang1 nodeID={12} name={"dinning"} location={"main-floor"} /> */}
 
       {myDevice.map((item) => (
-        <Gang1 key={item.id} nodeID={item.nodeID} name={item.name} location={item.location} />
-      ))}
-      {myDeviceWithEpID.map((item) => (
-        <Gang2 key={item.id} nodeID={item.nodeID} name={item.name} location={item.location} epID={item.epID} />
+        <Gang1
+          key={item.id}
+          nodeID={item.nodeID}
+          name={item.name}
+          location={item.location}
+          totalepID={item.totalepID}
+        />
       ))}
     </div>
   );
