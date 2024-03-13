a9fa42b8b3bb61c0ebec76e6ecbbc0459c034252
Ofir <ofirpatishop@gmail.com>
2024-03-13 23:02:04 +0200

    Reorder imports in index.tsx

--- React/cryptocurrency/src/index.tsx
+++ React/cryptocurrency/src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 import Main from "./Components/Layout/Main/Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
