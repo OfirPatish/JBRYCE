37931d634bda220ed54f49d04981a8ce757b4d14
Ofir Patish <ofirpatishop@gmail.com>
2024-03-13 23:00:03 +0200

    Fix import statement in index.tsx

--- React/cryptocurrency/src/index.tsx
+++ React/cryptocurrency/src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Main from "./Components/Layout/Main/Main";
 import { BrowserRouter } from "react-router-dom";
+import Main from "./Components/Layout/Main/Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
