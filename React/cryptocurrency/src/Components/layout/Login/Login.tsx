160ef7e40330948bcef69dbdac05bf135b6dea18
Ofir <ofirpatishop@gmail.com>
2024-03-16 21:44:35 +0200

    Add username and password state variables and reset functionality in Login component

--- React/cryptocurrency/src/Components/layout/Login/Login.tsx
+++ React/cryptocurrency/src/Components/layout/Login/Login.tsx
@@ -1,9 +1,18 @@
 import { Button, TextField, Checkbox, FormControlLabel, Container, Box, Grid, InputAdornment } from "@mui/material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import Lock from "@mui/icons-material/Lock";
+import { useState } from "react";
 import "./Login.css";
 
 function Login(): JSX.Element {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleReset = () => {
+    setUsername("");
+    setPassword("");
+  };
+
   return (
     <Container maxWidth="xs">
       <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -15,6 +24,8 @@ function Login(): JSX.Element {
               label="Username"
               variant="outlined"
               fullWidth
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
@@ -30,6 +41,8 @@ function Login(): JSX.Element {
               label="Password"
               variant="outlined"
               fullWidth
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
@@ -45,14 +58,19 @@ function Login(): JSX.Element {
             </Box>
           </Grid>
           <Grid container spacing={2} justifyContent="center">
-            <Grid item xs={5}>
+            <Grid item xs={3}>
               <Button type="submit" variant="contained" fullWidth>
                 Submit
               </Button>
             </Grid>
-            <Grid item xs={5}>
-              <Button type="reset" variant="contained" fullWidth>
+            <Grid item xs={3}>
+              <Button type="button" variant="contained" fullWidth onClick={handleReset}>
                 Reset
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button type="button" variant="contained" fullWidth>
+                Register
               </Button>
             </Grid>
           </Grid>
