cb47ab912cb5f8c16b8f2cd1a6a323cfc898394f
Ofir <ofirpatishop@gmail.com>
2024-03-17 17:31:38 +0200

    Refactor login form component

--- React/cryptocurrency/src/Components/layout/Login/Login.tsx
+++ React/cryptocurrency/src/Components/layout/Login/Login.tsx
@@ -1,80 +1,85 @@
-import { Button, TextField, Checkbox, FormControlLabel, Container, Box, Grid, InputAdornment } from "@mui/material";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import Lock from "@mui/icons-material/Lock";
-import { useState } from "react";
-import "./Login.css";
+import { Button, TextField, Checkbox, FormControlLabel, Container, Box, Grid } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+interface FormInputs {
+  username: string;
+  password: string;
+  remember: boolean;
+}
 
 function Login(): JSX.Element {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
 
-  const handleReset = () => {
-    setUsername("");
-    setPassword("");
-  };
+  const onSubmit = (data: FormInputs) => console.log(data);
 
   return (
     <Container maxWidth="xs">
-      <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "#fafafa",
+          padding: 3,
+          borderRadius: 2,
+          boxShadow: 1,
+        }}
+      >
         <h1>Login</h1>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              type="text"
-              label="Username"
-              variant="outlined"
-              fullWidth
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              type="password"
-              label="Password"
-              variant="outlined"
-              fullWidth
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Lock />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} container justifyContent="center">
-            <Box mb={2}>
-              <FormControlLabel control={<Checkbox />} label="Remember me" />
-            </Box>
-          </Grid>
-          <Grid container spacing={2} justifyContent="center">
-            <Grid item xs={3}>
-              <Button type="submit" variant="contained" fullWidth>
-                Submit
-              </Button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                type="text"
+                label="Username"
+                variant="outlined"
+                fullWidth
+                {...register("username", {
+                  required: "Username is required",
+                  minLength: { value: 3, message: "Username must be at least 3 characters long" },
+                })}
+                error={Boolean(errors.username)}
+                helperText={errors.username?.message}
+              />
             </Grid>
-            <Grid item xs={3}>
-              <Button type="button" variant="contained" fullWidth onClick={handleReset}>
-                Reset
-              </Button>
+            <Grid item xs={12}>
+              <TextField
+                type="password"
+                label="Password"
+                variant="outlined"
+                fullWidth
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: { value: 6, message: "Password must be at least 6 characters long" },
+                })}
+                error={Boolean(errors.password)}
+                helperText={errors.password?.message}
+              />
             </Grid>
-            <Grid item xs={3}>
-              <Button type="button" variant="contained" fullWidth>
-                Register
-              </Button>
+            <Grid item xs={12}>
+              <Box mb={2}>
+                <FormControlLabel control={<Checkbox {...register("remember")} />} label="Remember me" />
+              </Box>
+            </Grid>
+            <Grid container justifyContent="center" spacing={2}>
+              <Grid item xs={4}>
+                <Button type="submit" variant="contained" color="primary" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button type="button" variant="contained" color="success" fullWidth>
+                  Sign Up
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        </form>
       </Box>
     </Container>
   );
